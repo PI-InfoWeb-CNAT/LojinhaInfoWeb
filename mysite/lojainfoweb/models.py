9bbbd43e570ad614dd3d898d9e574f1140685a5d
Fellipe A. Aleixo <fellipe.aleixo@ifrn.edu.br>
2025-10-13 12:05:13 -0300

    Versão inicial das classes de modelo (parcial).

--- mysite/lojainfoweb/models.py
+++ mysite/lojainfoweb/models.py
@@ -1,18 +1,85 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-class Cliente(models.Model):
+#####
+## 1. Classe Usuário
+class Usuario(models.Model):
     TIPO = {
-        'a': 'Aluno de InfoWeb',
-        'd': 'Aluno ou servidor da DIATINF',
-        'i': 'Aluno ou servidor do IFRN',
-        'o': 'Outros...'
+        1: 'Administrador',
+        2: 'Gerente',
+        3: 'Aluno InfoWeb',
+        4: 'DIATINF',
+        5: 'IFRN',
+        9: 'Outros'
     }
-    tipo = models.IntegerField(choices=TIPO, default='o')
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    tipo = models.IntegerField(choices=TIPO, default=9)
     valido = models.BooleanField(default=False)
-    endereco = models.CharField(maxlength=100)
+    def __str__(self):
+        return self.user.username
 
+
+#####
+## 2. Classe Comprador
+class Comprador(Usuario):
+    endereco = models.CharField(maxlength=100)
+    def __str__(self):
+        return 'Comprador: {}'.format(super())
+
+
+#####
+## 3. Classe Categoria
+class Categoria(models.Model):
+    nome = models.CharField(max_length=30)
+    def __str__(self):
+        return self.nome
+
+
+#####
+## 4. Classe Produto
+class Produto(models.Model):
+    nome = models.CharField(max_length=50)
+    imagem = models.ImageField(upload_to='produtos')
+    descricao = models.models.CharField(max_length=100)
+    preco = models.DecimalField(max_digits=15, decimal_places=2)
+    quant_estoque = models.IntegerField(default=0)
+    categoria = models.ForeignKey(Categoria, default=None, null=True, on_delete=models.SET_NULL)
+    def __str__(self):
+        return self.nome
+
+
+#####
+## 5. Classe Reserva
+class Reserva(models.Model):
+    data = models.DateField(auto_now_add=True)
+    quantidade = models.IntegerField(default=1)
+    comprador = models.ForeignKey(Comprador, on_delete=models.CASCADE)
+    proxima = models.OneToOneField('Reserva', null=True, default=None)
+    def __str__(self):
+        return 'Reserva de {} para {}'.format(self.fila_reserva.produto.nome, self.comprador.user.username)
+
+
+#####
+## 6. Classe Fila de Reservas
+class FilaReserva(models.Model):
+    produto = models.OneToOneField(Produto)
+    inicio = models.OneToOneField(Reserva, null=True, blank= True, default=None)
+    fim = models.OneToOneField(Reserva, null=True, blank=True, default=None)
+    def __str__(self):
+        return 'Fila de reservas para {}'.format(self.produto.nome)
+    def enfileirar(self, reserva):
+        if self.inicio == None and self.fim == None:
+            self.inicio = reserva
+            self.fim = reserva
+            self.save()
+        else:
+            self.fim.proximo = reserva
+            self.fim.save()
+            self.fim = reserva
+            self.save()
+    
+#####
+## 7. Classe Compra
 class Compra(models.Model):
     ESTADO = {
         1: 'Carrinho de compras',
@@ -23,4 +90,16 @@ class Compra(models.Model):
     }
     estado = models.IntegerField(choices=ESTADO, default=1)
     data = models.DateField(auto_now_add=True)
-    cliente = models.ForeignKey(Cliente, null=True, on_delete=models.SET_NULL)
+    comprador = models.ForeignKey(Comprador, null=True, on_delete=models.SET_NULL)
+    def __str__(self):
+        return 'Compra de {} em {}'.format(self.comprador, self.data)
+
+#####
+## 8. Classe ItemDeCompra
+class ItemDeCompra(models.Model):
+    quantidade = models.IntegerField(default=1)
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    compra = models.ForeignKey(Compra, on_delete=models.CASCADE)
+    def __str__(self):
+        return 'Item da compra #{}'.format(self.compra.id)
+
